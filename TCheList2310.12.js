a51a90a7c9a97a3e4e09b34c10a24e7892f9e30c
Ivan Cherednichenko <114457127+IvanCherednichenko@users.noreply.github.com>
2023-10-12 15:38:59 +1200

    Create TCheList2310.12.js
    [ru]
    Подобие класса TStringList языка Pascal/Delphi, реализованный на JavaScript.
    
    [en]
    TCheList is simple clone of Pascal/Delphi's TStringList, which realized by JavaScript.

---
+++ TCheList2310.12.js
@@ -0,0 +1,68 @@
+﻿//
+// TCheList2310.12.js
+//
+// Cherednichenko TCheList version 2310.12.
+//
+// https://github.com/IvanCherednichenko/jsc
+//
+// (C) 2023 Ivan Cherednichenko. All Rights Reserved.
+//
+// This program is free software and distributed under GNU Lesser GPL 3.0 license.
+//
+
+function TCheList() {
+	var FNames = [];
+	var FValues = [];
+
+	this.indexOf = function(name, ignoreCase) {
+		if (ignoreCase) {
+			name = name.toLocaleLowerCase();
+		}
+
+		for (var i = 0; i < FNames.length; i++) {
+			if (FNames[i] == name) {
+				return i;
+			}
+			if (ignoreCase && (FNames[i].toLocaleLowerCase() == name)) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	this.add = function(name, value, modify) {
+		var i = this.indexOf(name);
+		if (i == -1) {
+			FNames.push(name);
+			return FValues.push(value);
+		}
+
+		if (modify) {
+			FValues[i] = value;
+			return i;
+		}
+	}
+
+	this.values = function(nameOrIndex, ignoreCase) {
+		if (Number.isInteger(nameOrIndex)) {
+			return FValues[nameOrIndex];
+		}
+
+		return FValues[this.indexOf(nameOrIndex, ignoreCase)];
+	}
+
+	this.delete = function(nameOrIndex) {
+		if (Number.isInteger(nameOrIndex)) {
+			if ((nameOrIndex >= 0) && (nameOrIndex <= FNames.length)) {
+				FNames.splice(nameOrIndex, 1);
+				FValues.splice(nameOrIndex, 1);
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+
+		return this.delete(this.indexOf(nameOrIndex));
+	}
+}
